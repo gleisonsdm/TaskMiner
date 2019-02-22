62b316f145a9e2750fe46b0aa17f07a81d44c0f0
gleisonsdm <gleison14051994@gmail.com>
2019-02-22 20:12:18 -0300

    Update processing_with_metric.r

--- scripts/R_scripts/processing_with_metric.r
+++ scripts/R_scripts/processing_with_metric.r
@@ -8,17 +8,25 @@ runtime_data <- runtime_data_tmp[order(runtime_data_tmp$Speedup),]
 indexS <- paste("S", seq(1,5), sep = "")
 indexP <- paste("P", seq(1,5), sep = "")
 
-BENCH <- runtime_data[,1]
+BENCH <- c()
+for (i in 1:83) {
+    tmp <- format(i)
+    while(nchar(tmp) < 3){
+      tmp <- paste("0", tmp, sep="")
+    }
+    BENCH <- c(BENCH, paste(tmp, runtime_data[i,1], sep=" - "))
+}
 BENCH <- cbind(BENCH,log(runtime_data[,indexP] / runtime_data[,indexS]))
-metric = gather(BENCH, key='id', value='value', 2, 3, 4, 5, 6)
-agg1 <- aggregate(value ~ BENCH, metric, function (x) c(Mean=mean(x), Sd=sd(x), Min=min(x), Max=max(x)))
-agg1 <- cbind(agg1, runtime_data[,"Speedup"])
+BENCH <- cbind(BENCH,log(runtime_data[,"Speedup"]))
+colnames(BENCH)[length(colnames(BENCH))] <- "Speedup"
+metric = gather(BENCH, key='id', value='value', 2, 3, 4, 5, 6, 7)
+
+agg1 <- aggregate(value ~ BENCH, metric, function (x) c(Mean=mean(x), Sd=sd(x), Min=min(x), Max=max(x), Speedup=x[6]))
+               
 agg1$value = as.data.frame(agg1$value)
-colnames(agg1)[length(colnames(agg1))] <- "Speedup"
-p = ggplot() + geom_boxplot(data=agg1, aes(x = BENCH, ymin = value$Min, ymax = value$Max, lower = Speedup - value$Sd, upper = Speedup + value$Sd, middle = Speedup), stat = "identity") + coord_flip()  +
+agg1 <- agg1[order(agg1$value$Speedup),]
+                  
+p = ggplot() + geom_boxplot(data=agg1, aes(x = agg1$BENCH, ymin = value$Min, ymax = value$Max, lower = value$Speedup - value$Sd, upper = value$Speedup + value$Sd, middle = value$Speedup), stat = "identity") + coord_flip()  +
   theme(axis.text.y = element_text(lineheight = 0.5
                                    , size = 6))
 ggsave("boxplot.pdf", path="Desktop/TaskMiner/scripts/R_scripts", plot = p, device="pdf", limitsize = FALSE)
-
-                  
-                  
